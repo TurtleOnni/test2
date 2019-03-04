47a4c4670da33feeb6d0170f74d173fe946c1936
TurtleOnni <katrik5@yandex.ru>
2019-03-04 15:46:15 +0300

    big commit

---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+//Test tree-view
+
+//to install node_modules
+    npm install
+
+// to run
+    npm run dev
+
+//to build
+    npm run build
---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { OrderByPipe } from './orderby.pipe';
+
+class Item{
+    name: string;
+    type: string;
+    isExpanded: boolean;
+    children: Item[]
+
+    constructor(_name: string, _type: string, _children: Item[]) {
+
+        this.name = _name;
+        this.type = _type;
+        this.isExpanded = false;
+        this.children = _children;
+    }
+}
+
+@Component({
+    selector: 'tree-view',
+    template: `<ul>
+                <li *ngFor="let item of Items| orderBy:name">
+                        <input type="checkbox" class="chControl" checked="item.isOpen"
+                               *ngIf="item.type==='folder' && item.children!=='undefined' && item.children.length!==0">
+                        <span title="{{item.type}}">
+			            {{item.name}} 
+		                </span>
+                    <span *ngIf="item.children!==undefined">({{item.children.length}})</span>
+                    <div class="isOpen">
+                        <tree-view [Items]="item.children" *ngIf="item.children!=='undefined'"></tree-view>
+                    </div>
+                </li>
+    </ul>`,
+    styles:['input[type=checkbox].chControl:checked ~ div.isOpen{ display:block } .isOpen {display: none}'],
+})
+export class TreeComponent {
+    @Input() Items: Item[];
+
+    constructor() { }
+
+}
